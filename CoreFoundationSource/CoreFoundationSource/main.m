cf25f08e59ddd22fac2379bd06b85e336473819f
luoquan <nchkdxlq.gmail.com>
2018-07-01 12:07:01 +0800

    add CF source code

---
+++ CoreFoundationSource/CoreFoundationSource/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CoreFoundationSource
+//
+//  Created by nchkdxlq on 2018/7/1.
+//  Copyright © 2018年 luoquan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
